20a927c879c28c4f5e129a90f7d6bdde39b85298
Rodrigo gentini <gentini@msn.com>
2021-05-31 14:03:00 -0300

    Calculation and Refactor

--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 
+// usando template engine
+server.set('view engine', 'ejs')
+
 // habilitar arquivos statics
 server.use(express.static("public"))
+
+// usar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // rotas
 server.use(routes)
